f907a61fb1d181f84d8dc86d009f9ff5de2e2d92
Luis Arnaut <arnaut@Arnaut-MacBook-Pro.local>
2021-07-15 15:44:44 -0700

    added functionality to add pizzas

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 const htmlRoutes = require('./html/html-routes');
+const apiRoutes = require('./api');
 
 router.use('/', htmlRoutes);
+// add previx of /api to all the api routes imported
+router.use('/api', apiRoutes);
 
 router.use((req, res) => {
   res.status(404).send('<h1>😝 404 Error!</h1>');
 });
+
 
 module.exports = router;
